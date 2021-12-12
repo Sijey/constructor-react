54a3a979210d327d3593d1ddda004b855f774de3
Andrew Popov <andrew.pp@darly.solutions>
2021-12-12 20:08:40 +0200

    constructor component

---
+++ src/components/Constructor.jsx
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+
+const Constructor = () => {
+  const key = '5u817CheLB9JUwhHFDW6QH58';
+  const getProducts = () => {
+    fetch('api.keepincrm.com/v1/products', {
+      headers: {
+        Name: 'X-Auth-Token',
+        Value: key,
+      },
+    })
+        .then(res => {
+          console.log(res)
+        })
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+      <div>
+
+      </div>
+  );
+};
+
+export default Constructor;
